f0b5a18162ea7ba52e696b0c3488eda5371a5557
WBomfim <willianbomfim@hotmail.com>
2023-02-11 15:48:36 -0300

    feat: Criado as funções read_file e format_data

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,21 @@
+def read_file(path_to_file):
+    if not path_to_file.endswith(".csv"):
+        raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
+
+    try:
+        with open(path_to_file) as file:
+            return list(file.read().splitlines())
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
+
+
+def format_data(path_to_file):
+    lines = read_file(path_to_file)
+    return [tuple(line.split(",")) for line in lines]
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    data = format_data(path_to_file)
+    for client, product, day in data:
+        print(client)
