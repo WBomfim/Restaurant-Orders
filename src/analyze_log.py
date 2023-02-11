031fdf9bba6680de7eb0902e309cb8684a1cf843
WBomfim <willianbomfim@hotmail.com>
2023-02-11 16:36:48 -0300

    feat: Criado a função get_information_from_data

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -15,7 +15,25 @@ def format_data(path_to_file):
     return [tuple(line.split(",")) for line in lines]
 
 
+def get_information_from_data(data):
+    products = set()
+    days = set()
+    clients = {}
+
+    for client, product, day in data:
+        products.add(product)
+        days.add(day)
+
+        if client not in clients:
+            clients[client] = {"products": [], "days": []}
+
+        clients[client]["products"].append(product)
+        clients[client]["days"].append(day)
+
+    return products, days, clients
+
+
 def analyze_log(path_to_file):
     data = format_data(path_to_file)
-    for client, product, day in data:
-        print(client)
+    products, days, clients = get_information_from_data(data)
+    return print(products, days, clients)
