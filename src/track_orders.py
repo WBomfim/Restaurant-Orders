d61dc7abd0f419a6480fd95a1d48db60a4c701a3
WBomfim <willianbomfim@hotmail.com>
2023-02-11 22:27:06 -0300

    feat: Adicionado os atributos _products_available e _worked_days a classe TrackOrders - Os atributos tem como objetivo auxiliar nas análises solicitadas

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,13 +1,20 @@
 class TrackOrders:
     _orders: list
+    _products_available: set
+    _worked_days: set
 
     def __init__(self):
         self._orders = []
+        self._products_available = set()
+        self._worked_days = set()
 
     def __len__(self):
         return len(self._orders)
 
     def add_new_order(self, customer, order, day):
+        self._products_available.add(order)
+        self._worked_days.add(day)
+
         for register in self._orders:
             if register["customer"] == customer:
                 register["order"].append(order)
